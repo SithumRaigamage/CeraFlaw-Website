00f99cf203b7b6558d447f99a92702f4788c0ab1
Thamindu Dasanayake <136196555+thamindu2003@users.noreply.github.com>
2024-03-03 00:38:39 +0530

    made the website responsive

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*==================== toggle icon navbar ====================*/ 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
 /*================ scroll section active link ================*/ 
 
 let sections = document.querySelectorAll('section');
